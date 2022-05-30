1582665170896c2512c0b9858ac3fa9663f2c169
istmountain <istmountain@gmail.com>
2022-05-30 22:04:14 +0200

    hakan branchinda 2. degisiklik

--- src/kisiselkullanim/githabkullanim.java
+++ src/kisiselkullanim/githabkullanim.java
@@ -3,10 +3,8 @@ package kisiselkullanim;
 public class githabkullanim {
     public static void main(String[] args) {
           System.out.println("hello world");
-        System.out.println("thats it");
-
-
-
+          System.out.println("thats it");
+          System.out.println("ahmet branchinda ortak classta degisiklik yaptim");
 
     }
 }
--- src/kisiselkullanim/ozellik1.java
+++ src/kisiselkullanim/ozellik1.java
@@ -1,7 +1,10 @@
 package kisiselkullanim;
 
+import org.w3c.dom.ls.LSOutput;
+
 public class ozellik1 {
     public static void main(String[] args) {
         System.out.println("hakan branchinda calisiyorum");
+        System.out.println("hakan branchinda 2. degisiklik kendime ozel classta");
     }
 }
